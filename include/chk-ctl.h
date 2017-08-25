7d1864d0c2c6af610f09b42270d61715cb8d0f97
Svetlana Linuxenko <svetlana@linuxenko.pro>
2017-08-25 12:09:20 +0300

    LICENSE

--- include/chk-ctl.h
+++ include/chk-ctl.h
@@ -1,3 +1,23 @@
+/*
+ *    chkservice is a tool for managing systemd units.
+ *    more infomration at https://github.com/linuxenko/chkservice
+ *
+ *    Copyright (C) 2017 Svetlana Linuxenko <svetlana(at)linuxenko.pro>
+ *
+ *    chkservice program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    chkservice program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _CHK_CTL_H
 #define _CHK_CTL_H
 
@@ -30,6 +50,7 @@ class ChkCTL {
   public:
     ChkCTL();
     ~ChkCTL();
+    ChkBus *bus;
     std::vector<UnitItem *> getItemsSorted();
     std::vector<UnitItem *> getByTarget(const char *target);
     std::vector<UnitItem *> getItems();
@@ -37,7 +58,6 @@ class ChkCTL {
     void toggleUnitSubState(UnitItem *item);
     void fetch();
   private:
-    ChkBus *bus;
     std::vector<UnitItem *> items;
     void pushItem(UnitInfo unit);
     void sortByName(std::vector<UnitItem *> *sortable);
--- include/chk.h
+++ include/chk.h
@@ -1,3 +1,23 @@
+/*
+ *    chkservice is a tool for managing systemd units.
+ *    more infomration at https://github.com/linuxenko/chkservice
+ *
+ *    Copyright (C) 2017 Svetlana Linuxenko <svetlana(at)linuxenko.pro>
+ *
+ *    chkservice program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    chkservice program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef _CHK_H
 #define _CHK_H
 
@@ -18,10 +38,10 @@
 \n\
   Action keys:\n\
 \n\
-    ? - help.                q - quit.\n\
+    r     - reload/update.   q - exit.\n\
     Space - enable/disable.  s - start/stop unit.\n\
 \n\
   License:\n\
-    GPLv3 (c) Svetlana Linuxenko <svetlana@linuxenko.pro>"
+    GPLv3 (c) Svetlana Linuxenko <svetlana(at)linuxenko.pro>"
 
 #endif
